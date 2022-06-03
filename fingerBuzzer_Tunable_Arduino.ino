0fa9dadd4a1a638bfd451082c70b2305d9195e12
CadeTyler <98047022+CadeTyler@users.noreply.github.com>
2022-06-02 22:37:42 -0400

    Update fingerBuzzer_Tunable_Arduino.ino

--- fingerBuzzer_Tunable_Arduino.ino
+++ fingerBuzzer_Tunable_Arduino.ino
@@ -1,22 +1,23 @@
 const byte numChars = 20;
 char dataIn[numChars];
-
 const short pins[] = {2,3,6,9,12};
-//change pins[] to reflect actual wiring of arduino
-
+//const short pins[] = {6,5,4,3,2};
 char startMarker = '<';
 char endMarker = '>';
+
+#define ON 255 //max value of 255
+#define OFF 0
 
 void setup() {
   Serial.begin(9600);
 
   for (int i = 0; i < 5; i++) {
   pinMode(pins[i], OUTPUT);
-  digitalWrite(pins[i], 0);
+  analogWrite(pins[i], OFF);
   delay(5);
-  digitalWrite(pins[i], 1);
+  analogWrite(pins[i], ON);
   delay(300);
-  digitalWrite(pins[i], 0);
+  analogWrite(pins[i], OFF);
   }
 
   delay(100);
@@ -27,9 +28,9 @@ void loop() {
 
   for (int i = 0; i < 5; i++) {
     if (dataIn[i] == '1') {
-      digitalWrite(pins[i], 1);
+      analogWrite(pins[i], ON);
     } else {
-      digitalWrite(pins[i], 0);
+      analogWrite(pins[i], OFF);
     }
   }
 
